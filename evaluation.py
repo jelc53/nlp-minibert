f71ea9b2f5f55315034951e9f2dad671e9e34441
Julian Cooper <jelc@stanford.edu>
2023-03-01 16:07:01 -0800

    reverted data split arg passed to load_multitask_data

--- evaluation.py
+++ evaluation.py
@@ -227,7 +227,7 @@ def model_eval_test_multitask(sentiment_dataloader,
 
 
 def test_model_multitask(args, model, device):
-        sst_test_data, num_labels,para_test_data, sts_test_data = \
+        sst_test_data, num_labels, para_test_data, sts_test_data = \
             load_multitask_data(args.sst_test,args.para_test, args.sts_test, split='test')
 
         sst_dev_data, num_labels,para_dev_data, sts_dev_data = \
--- multitask_classifier.py
+++ multitask_classifier.py
@@ -154,7 +154,7 @@ def train_multitask(args):
     # Load data
     # Create the data and its corresponding datasets and dataloader
     sst_train_data, num_labels,para_train_data, sts_train_data = load_multitask_data(args.sst_train,args.para_train,args.sts_train, split ='train')
-    sst_dev_data, num_labels,para_dev_data, sts_dev_data = load_multitask_data(args.sst_dev,args.para_dev,args.sts_dev, split ='test')
+    sst_dev_data, num_labels,para_dev_data, sts_dev_data = load_multitask_data(args.sst_dev,args.para_dev,args.sts_dev, split ='train')
 
     sst_train_data = SentenceClassificationDataset(sst_train_data, args)
     sst_dev_data = SentenceClassificationDataset(sst_dev_data, args)
