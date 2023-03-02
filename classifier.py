31b0754e00f4c27b2d546d5213eae9d944ba2e93
Julian Cooper <jelc@stanford.edu>
2023-03-02 11:14:37 -0800

    return extension default to default

--- classifier.py
+++ classifier.py
@@ -367,7 +367,7 @@ def get_args():
     parser.add_argument("--batch_size", help='sst: 64, cfimdb: 8 can fit a 12GB GPU', type=int, default=8)
     parser.add_argument("--hidden_dropout_prob", type=float, default=0.3)
     parser.add_argument("--lr", type=float, help="learning rate, default lr for 'pretrain': 1e-3, 'finetune': 1e-5", default=1e-5)
-    parser.add_argument("--extension", type=str, default="smart")
+    parser.add_argument("--extension", type=str, default="default")
 
     # adversarial regularization
     parser.add_argument('--pgd_k', type=int, default=1)
